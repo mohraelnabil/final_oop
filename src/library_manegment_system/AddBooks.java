036dc651421acd42bf099850513300df4cbf0b48
shahdabdallah2004 <138774111+shahdabdallah2004@users.noreply.github.com>
2023-12-14 00:35:34 +0200

    AddBooks.java

--- src/library_manegment_system/AddBooks.java
+++ src/library_manegment_system/AddBooks.java
@@ -1,8 +1,30 @@
-package library_manegment_system;
+package library;
+
+import java.util.Scanner;
 
 public class AddBooks implements IOOperation{
 	@Override
 	public void oper(Database database, User user) {
+		
+		Scanner s = new Scanner (System.in);
+		Book book = new Book();
+		System.out.println("Enter book name: ");
+		book.setName(s.next());
+		System.out.println("Enter book Author: ");
+		book.setAuthor(s.next());
+		System.out.println("Enter book Publisher: ");
+		book.setPublisher(s.next());
+		System.out.println("Enter book Collection Address: ");
+		book.setAdress(s.next());
+		System.out.println("Enter book Qty: ");
+		book.setQty(s.nextInt());
+		System.out.println("Enter book Price: ");
+		book.setPrice(s.nextDouble());
+		System.out.println("Enter Borrowing coppies: ");
+		book.setBrwcopies(s.nextInt());
+		s.close();
+		database.AddBook(book);
+		System.out.println("Book added successfully!");
 		
 	}
 
