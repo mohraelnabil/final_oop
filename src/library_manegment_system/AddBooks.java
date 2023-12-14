a3febfeed1aaad4db6d05892ab0668d229d7b0aa
shahdabdallah2004 <138774111+shahdabdallah2004@users.noreply.github.com>
2023-12-14 22:18:37 +0200

    Update AddBooks.java

--- src/library_manegment_system/AddBooks.java
+++ src/library_manegment_system/AddBooks.java
@@ -8,15 +8,8 @@ public class AddBooks implements IOOperation{
 		
 		Scanner s = new Scanner (System.in);
 		Book book = new Book();
-		System.out.println("\nEnter book name: ");
-		String name=s.next();
-		if(database.getBook(name)>-1) {
-			System.out.println("There is a book with this name!\n");
-			user.menu(database,user);
-			return;
-		}
-		else {
-		book.setName(name);
+		System.out.println("Enter book name: ");
+		book.setName(s.next());
 		System.out.println("Enter book Author: ");
 		book.setAuthor(s.next());
 		System.out.println("Enter book Publisher: ");
@@ -27,14 +20,10 @@ public class AddBooks implements IOOperation{
 		book.setQty(s.nextInt());
 		System.out.println("Enter book Price: ");
 		book.setPrice(s.nextDouble());
-		System.out.println("Enter Borrowing coppies: ");
-		book.setBrwcopies(s.nextInt());
 		s.close();
 		database.AddBook(book);
-		System.out.println("Book added successfully!\n");
-		user.menu(database,user);
-		s.close();
-		}
+		System.out.println("Book added successfully!");
+		
 	}
 
 
