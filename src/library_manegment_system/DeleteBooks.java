52d8a81bd5ab8f0768c56f8660bfc063e0f60ea0
hanagamal7 <141966923+hanagamal7@users.noreply.github.com>
2023-12-13 17:48:08 -0800

    Update DeleteBooks.java

--- src/library_manegment_system/DeleteBooks.java
+++ src/library_manegment_system/DeleteBooks.java
@@ -1,9 +1,26 @@
 package library;
 
+import java.util.Scanner;
+
 public class DeleteBooks implements IOOperation{
 	@Override
 	public void oper(Database database, User user) {
 		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter book name: ");
+		String bookname = s.next();
+		
+		int i=database.getBook(bookname);
+		if(i>-1) {
+			database.deleteBook(i);
+			System.out.println("Book deleted successfully!\n");
+		}
+		else {
+			System.out.println("Book doesn't exist!\n");
+		}
+		
+
+		user.menu(database,user);
 	}
 
 }
