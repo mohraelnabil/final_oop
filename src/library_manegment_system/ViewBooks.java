ca4f1178083b18d088029b620186ee505dddf8b0
hanagamal7 <141966923+hanagamal7@users.noreply.github.com>
2023-12-13 17:51:52 -0800

    Update ViewBooks.java

--- src/library_manegment_system/ViewBooks.java
+++ src/library_manegment_system/ViewBooks.java
@@ -1,9 +1,18 @@
 package library;
+import java.util.ArrayList; 
 
 public class ViewBooks implements IOOperation{
 	@Override
 	public void oper(Database database, User user) {
-
+     
+		ArrayList<Book> books = database.getAllBooks();
+		System.out.println("Name\t\tAuthor\t\tPublisher\tCLA\tQty\tPrice" +"\tBrw cps");
+		for(Book b : books) {
+			System.out.println(b.getName()+"\t\t"+b.getAuthor()+"\t\t"+b.getPublisher()+
+					"\t\t"+b.getAdress()+"\t"+b.getQty()+"\t"+b.getPrice()+"\t"+b.getBrwcopies());
+		}
+		System.out.println();
+		user.menu(database,user);
 	}
 
 }
